3fed9cd876eea53776f873eb9dcc5186b408c3f0
Lucas Magno <lmagno94@gmail.com>
2016-03-10 15:19:10 -0300

    Use variables instead of pointers

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     signal(SIGINT, handler);
-    QSerialPort *serial = new QSerialPort();
+    QSerialPort serial;
 
     QFile in("device.cfg");
     if(!in.open(QIODevice::ReadOnly | QIODevice::Text)) {
@@ -28,24 +28,24 @@ int main(int argc, char *argv[])
         return 1;
     }
     QTextStream device(&in);
-    serial->setPortName(device.readLine());
-    serial->setBaudRate(QSerialPort::Baud9600);
-    serial->setDataBits(QSerialPort::Data8);
-    serial->setParity(QSerialPort::NoParity);
-    serial->setStopBits(QSerialPort::OneStop);
-    serial->setFlowControl(QSerialPort::NoFlowControl);
+    serial.setPortName(device.readLine());
+    serial.setBaudRate(QSerialPort::Baud9600);
+    serial.setDataBits(QSerialPort::Data8);
+    serial.setParity(QSerialPort::NoParity);
+    serial.setStopBits(QSerialPort::OneStop);
+    serial.setFlowControl(QSerialPort::NoFlowControl);
 
-    QXBee *xb = new QXBee(serial);
-	QObject::connect(xb, SIGNAL(dataReceived(XBeePacket*)), xb, SLOT(displayData(XBeePacket*)));
+    QXBee xb(&serial);
+	QObject::connect(&xb, SIGNAL(dataReceived(XBeePacket*)), &xb, SLOT(displayData(XBeePacket*)));
 
     // QByteArray address = QByteArray::fromHex("0013a20040a53581");
     // QString data = "Hello World";
 
-	ATCommand *teste = new ATCommand;
-	teste->setFrameID(0x01);
-    teste->setATCommand("ND");
+	ATCommand teste;
+	teste.setFrameID(0x01);
+    teste.setATCommand("ND");
 
-    xb->send(teste);
+    xb.send(&teste);
 
     return a.exec();
 }
